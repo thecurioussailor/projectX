452bb6130d9bb5294063b3e74ccbe9447e2554f0
Ashutosh Sagar <48312681+thecurioussailor@users.noreply.github.com>
2025-05-20 23:52:10 +0530

    corrected search for mobile

--- apps/frontend/src/components/digitalProducts/DigitalProductTable.tsx
+++ apps/frontend/src/components/digitalProducts/DigitalProductTable.tsx
@@ -78,10 +78,10 @@ const DigitalProductTable = () => {
                     <div className="absolute rounded-full bg-[#FFC717] h-8 w-8 -top-12 -left-4"></div>
                     <div className="absolute rounded-full bg-[#06B5DD] h-4 w-4 top-3 -left-2"></div>
                 </div>
-                <div className="flex justify-between items-start px-12">
+                <div className="flex flex-col lg:flex-row justify-between items-start px-12">
                     <h1 className="text-2xl pb-10 font-bold px-12 text-[#1B3155]">Digital Products</h1>
                     {/* Search and Filter Section */}
-                    <div className="flex items-center gap-4">
+                    <div className="flex flex-col lg:flex-row items-center gap-4">
                         {/* Search Bar */}
                         <div className="relative">
                             <input
